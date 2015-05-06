3197d0960cb16ae79de626c5aa58f26908c77d78
zjh <zjh@cleantha.local>
2015-05-06 20:54:50 +0800

    fix some issue

--- index.js
+++ index.js
@@ -7,44 +7,22 @@ _.once = function(fn) {
 		if (!invoked) {
 			invoked = true;
 			result = fn();
-			return result;
-		} else {
-			return result;
 		}
-	}
-}
-
-_.memoizewithoutcallback = function(fn) {
-	var caches = {};
-	var funcname = fn.name;
-	console.log(funcname);
-	return function(value) {
-		var cachekey = funcname + "-" + value;
-		if (caches.hasOwnProperty(cachekey)) {
-			console.log("has property");
-			return caches[cachekey];
-		} else {
-			var result = fn(value);
-			caches[cachekey] = result;
-			console.log("no property");
-			return result;
-		}
+		return result;
 	}
 }
 
 _.memoize = function(fn, callback) {
 	var caches = {};
 	return function(value) {
-		var cachekey = callback(value);
-		if (caches.hasOwnProperty(cachekey)) {
-			console.log("has property");
-			return caches[cachekey];
-		} else {
-			var result = fn(value);
-			caches[cachekey] = result;
-			console.log("no property");
-			return result;
+		var cachekey = value;
+		if (callback) {
+			var cachekey = callback(value);
+		};
+		if (!caches.hasOwnProperty(cachekey)) {
+			caches[cachekey] = fn(value);
 		}
+		return caches[cachekey];
 	}
 }
 
@@ -55,3 +33,23 @@ _.bind = function (fn, scope) {
 }
 
 module.exports = _;
+
+var i = 0; // count how many times identity is called
+function identity(n) {
+	i = i + 1;
+	return n;
+}
+
+function cleantha(n) {
+	return n + 1;
+}
+
+//对于不同的外层参数 每个 memoize 返回的函数都有自己的 cache 也就是传入的fn不同 返回的闭包是独立的
+
+memoizedIdentity = _.memoize(identity);
+console.log(memoizedIdentity(1));
+console.log(memoizedIdentity(1));
+
+memoizeCleantha = _.memoize(cleantha);
+console.log(memoizeCleantha(1));
+console.log(memoizeCleantha(1));
